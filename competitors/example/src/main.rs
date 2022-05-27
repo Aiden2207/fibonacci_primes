f8d6665943a58f9fd163003646d775ab113d3f21
Aiden2207 <ajsandbakken@comcast.net>
2022-05-27 00:43:35 -0500

    fixed example

--- competitors/example/src/main.rs
+++ competitors/example/src/main.rs
@@ -1,8 +1,9 @@
 use num_bigint::BigUint;
 use std::io::prelude::*;
+use std::io::Result;
 //note: mostly written by github copilot
 fn main() -> Result<()> {
-    for (i, n) in fibonacci().zip(1u64..).filter(|(_, n)| is_prime(n)) {
+    for (n, i) in fibonacci().zip(1u64..).filter(|(n, _)| is_prime(n)) {
         let bytes = n.to_bytes_le();
         let mut lock = std::io::stdout();
         lock.write_all(&i.to_le_bytes())?;
@@ -29,30 +30,30 @@ impl Iterator for Fibonacci {
 
 fn fibonacci() -> impl Iterator<Item = BigUint> {
     Fibonacci {
-        curr: 1.into(),
-        next: 1.into(),
+        curr: 1u32.into(),
+        next: 1u32.into(),
     }
 }
 
 fn is_prime(n: &BigUint) -> bool {
-    if n < &2.into() {
+    if n < &2u32.into() {
         return false;
     }
-    if n == &2.into() || n == &3.into() {
+    if n == &2u32.into() || n == &3u32.into() {
         return true;
     }
-    if n % &2.into() == 0.into() {
+    if n % &2u32 == 0u32.into() {
         return false;
     }
-    if n % &3.into() == 0.into() {
+    if n % &3u32 == 0u32.into() {
         return false;
     }
-    let mut i = 5.into();
-    while i * i <= n {
-        if n % i == 0.into() || n % (i + 2.into()) == 0.into() {
+    let mut i = 5u32;
+    while n >= &(i * i).into() {
+        if n % i == 0u32.into() || n % (i + 2) == 0u32.into() {
             return false;
         }
-        i += 4.into();
+        i += 4u32;
     }
     true
 }
