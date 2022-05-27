e39b5320836e93c6655c5c1791bc591ba2035dc3
Aiden2207 <ajsandbakken@comcast.net>
2022-05-26 19:01:06 -0500

    added ReadTimeout

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,11 @@ use std::collections::BTreeMap;
 fn main() {
     let nums = println!("{}", fibonacci(25561));
 }
-const INDICES: [u32; 36] = [
+const INDICES: [u32; 51] = [
     3, 4, 5, 7, 11, 13, 17, 23, 29, 43, 47, 83, 131, 137, 359, 431, 433, 449, 509, 569, 571, 2971,
     4723, 5387, 9311, 9677, 14431, 25561, 30757, 35999, 37511, 50833, 81839, 104911, 130021,
-    148091,
+    148091, 201107, 397379, 433781, 590041, 593689, 604711, 931517, 1049897, 1285607, 1636007,
+    1803059, 1968721, 2904353, 3244369, 3340367,
 ];
 static FIBS: Lazy<BTreeMap<u32, BigUint>> =
     Lazy::new(|| INDICES.into_iter().map(|n| (n, fibonacci(n))).collect());
@@ -16,4 +17,41 @@ fn fibonacci(n: u32) -> BigUint {
     let mut mat = na::matrix![BigUint::from(1u32), 1u32.into(); 1u32.into(), 0u32.into()];
     mat.pow_mut(n - 1);
     mat[0].clone()
+}
+
+use std::io::ErrorKind;
+use std::io::Result;
+use std::pin::Pin;
+use std::task::{Context, Poll};
+use std::time::{Duration, Instant};
+use tokio::io::*;
+#[derive(Debug)]
+struct ReadTimeout<R> {
+    reader: R,
+    start: Instant,
+    timeout: Duration,
+}
+impl<R> ReadTimeout<R> {
+    fn new(reader: R, timeout: Duration) -> Self {
+        Self {
+            reader,
+            start: Instant::now(),
+            timeout,
+        }
+    }
+}
+
+impl<R: AsyncRead> AsyncRead for ReadTimeout<R> {
+    fn poll_read(
+        self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        buf: &mut ReadBuf<'_>,
+    ) -> Poll<Result<()>> {
+        if Instant::now() - self.start > self.timeout {
+            return Poll::Ready(Err(ErrorKind::TimedOut.into()));
+        }
+        let inner = unsafe { self.get_unchecked_mut() };
+        let reader = unsafe { Pin::new_unchecked(&mut inner.reader) };
+        reader.poll_read(cx, buf)
+    }
 }
