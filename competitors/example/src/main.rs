5e07c8b0bd6b828f2d90b07487e443dea792f2ec
Aiden2207 <ajsandbakken@comcast.net>
2022-05-27 01:12:49 -0500

    fixed example

--- competitors/example/src/main.rs
+++ competitors/example/src/main.rs
@@ -1,11 +1,10 @@
 use num_bigint::BigUint;
 use std::io::prelude::*;
 use std::io::Result;
-//note: mostly written by github copilot
 fn main() -> Result<()> {
     for (n, i) in fibonacci().zip(1u64..).filter(|(n, _)| is_prime(n)) {
         let bytes = n.to_bytes_le();
-        let mut lock = std::io::stdout();
+        let mut lock = std::io::stdout().lock();
         lock.write_all(&i.to_le_bytes())?;
         lock.write_all(&bytes.len().to_le_bytes())?;
         lock.write_all(&bytes)?;
@@ -21,10 +20,10 @@ pub struct Fibonacci {
 impl Iterator for Fibonacci {
     type Item = BigUint;
     fn next(&mut self) -> Option<Self::Item> {
-        let result = self.curr.clone();
-        self.curr = self.next.clone();
-        self.next += &self.curr;
-        Some(result)
+        let result = &self.next + &self.curr;
+        let tmp = std::mem::replace(&mut self.next, result);
+        let ret = std::mem::replace(&mut self.curr, tmp);
+        Some(ret)
     }
 }
 
